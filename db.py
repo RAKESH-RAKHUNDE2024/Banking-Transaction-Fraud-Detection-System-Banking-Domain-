461c4da6bcc6747f062cf76b6809f10fc8286d6a
Rakesh Rakhunde <153727258+RAKESH-RAKHUNDE2024@users.noreply.github.com>
2026-01-23 23:50:29 +0530

    update details

--- db.py
+++ db.py
@@ -1,18 +1,14 @@
 import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-MYSQL_USER = os.getenv("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "root")
-MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
-MYSQL_DB = os.getenv("MYSQL_DB", "fraud_db")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set. Please set it in Render Environment Variables.")
 
-DATABASE_URL = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
-engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
