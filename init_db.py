e1733cfd93a1a530fb17a31e55174edab91b2236
Rakesh Rakhunde <153727258+RAKESH-RAKHUNDE2024@users.noreply.github.com>
2026-01-24 13:49:09 +0530

    Initialize database and create tables

--- init_db.py
+++ init_db.py
@@ -1,1 +1,10 @@
+from db import engine
+from models import Base
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+    print("Database Tables Created Successfully!")
+
+if __name__ == "__main__":
+    init_db()
 
