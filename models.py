bfbf987ae98e4661812c671e2f7a24ec9060aac9
Rakesh Rakhunde <153727258+RAKESH-RAKHUNDE2024@users.noreply.github.com>
2026-01-24 13:46:37 +0530

    Refactor PredictionLog to Transaction model

--- models.py
+++ models.py
@@ -1,35 +1,19 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean
 from datetime import datetime
-from db import Base
 
-class PredictionLog(Base):
-    __tablename__ = "prediction_logs"
+Base = declarative_base()
+
+class Transaction(Base):
+    __tablename__ = "transactions"
 
     id = Column(Integer, primary_key=True, index=True)
 
-    # New Transaction ID
-    transaction_id = Column(String(20), unique=True, index=True, nullable=False)
+    transaction_id = Column(String(100), unique=True, nullable=False)
+    amount = Column(Float, nullable=False)
 
-    transaction_type = Column(String(50))
-    merchant_category = Column(String(50))
-    amount_inr = Column(Float)
-    transaction_status = Column(String(20))
+    sender_account = Column(String(50), nullable=True)
+    receiver_account = Column(String(50), nullable=True)
 
-    sender_age_group = Column(String(20))
-    receiver_age_group = Column(String(20))
-    sender_state = Column(String(50))
-
-    sender_bank = Column(String(50))
-    receiver_bank = Column(String(50))
-    device_type = Column(String(20))
-    network_type = Column(String(20))
-
-    hour_of_day = Column(Integer)
-    day_of_week = Column(String(20))
-    is_weekend = Column(Integer)
-
-    label = Column(String(10))
-    fraud_probability = Column(Float)
-    risk_level = Column(String(10))
-
+    is_fraud = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.utcnow)
